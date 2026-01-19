0b1a846e4f57924d0d1ec55cccdfa6b7e6b5f67d
AsaelC <victor_cg115@hotmail.com>
2026-01-18 21:57:47 -0500

    Finished faster than I thought lol

--- passwordValidator.py
+++ passwordValidator.py
@@ -12,31 +12,55 @@ from humre import *
 def passWordValidator(password):
     
     #Checking first that the password is at least 8 characters long
-    minimumLengthRegexExpression = at_least(8,DIGIT)
+    minimumLengthRegexExpression = at_least(8,ANYCHAR)
     lengthPattern = re.compile(minimumLengthRegexExpression)
-    '''lengthValidator = lengthPattern.search(password)
+    lengthValidator = lengthPattern.search(password)
     if lengthValidator == None:
-        print("Invalid Password :c")
-        return'''
+        print("Sorry, not enough characters \n \n")
+        return
 
-    #TODO contain both uppercase and lowercase characters
     
     
     
+    #Uppercase
+    upperRegex = at_least(1, UPPERCASE)
+    capitalPattern = re.compile(upperRegex)
+    upperValidator = capitalPattern.search(password)
+    if upperValidator == None:
+        print("Password must have at least 1 uppercase letter \n \n")
+        return
     
-    #TODO have at least one digit
+
+    #LowerCase
+    lowerRegex = at_least(1, LOWERCASE)
+    nonCapitalPattern = re.compile(lowerRegex)
+    lowerValidator = nonCapitalPattern.search(password)
+    if lowerValidator == None:
+        print("Password must have at least 1 lowercase letter \n \n")
+        return
+    
+    #At least one digit
+    digitRegex = at_least(1, DIGIT)
+    digitPattern = re.compile(digitRegex)
+    digitValidator = digitPattern.search(password)
+    if digitValidator == None:
+        print("Password must have at least 1 digit \n \n")
+        return
     
     
-    
-    print("Valid Password :D")
+    print("Valid Password :D \n \n")
     return 
-
 
 
 
 while True:
     
+    
+    
+    print("CTRL-C to exit")
     userInput= input("Please enter a Password :D \n")
-    print("Enter Taco to exit")
-    if user
+    
+    if userInput == None:
+        break
+    
     passWordValidator(userInput)
